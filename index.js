a4150e0164fb09b1de54ddb76d292b03afd5e014
Shivam7264 <91426482+Shivam7264@users.noreply.github.com>
2023-02-08 14:23:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
+        isLiked:false,
+        uuid:"1-w-1"
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +17,9 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        isLiked:false,
+        uuid:"1-w-2"
     },
         {
         name: "Joseph Ducreux",
@@ -24,7 +28,9 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        isLiked:false,
+        uuid:"1-w-3"
     },
     {
         name: "shivam Shukla",
@@ -33,43 +39,12 @@ const posts = [
         avatar: "images/new1.jpg",
         post: "images/prab.jpg",
         comment: " hey there ,lets ready for the party!! ",
-        likes: 1234
+        likes: 1234,
+        isLiked:false,
+        uuid:"1-w-4"
     }
 
 ]
-
-let postingEl=document.getElementById("posting")
-function renderImage(){
-    let temp=""
-for(let i=0;i<posts.length;i+=1){
-    temp+=`
-
-    <div class="user-info">
-        <div id="post-1-dp"><img  id="post-1-dp" src="${posts[i].avatar}"></div>
-        <div class="info">
-            <div id="fname">${posts[i].name}</div>
-            <div  id="location">${posts[i].location}</div>
-        </div>
-    </div>
-    
-    <div class="post-1-image"><img id="post-1"  src="${posts[i].post}" data-react="${posts[i].uuid}"></div>
-    <div id="react">
-    <img  class="react-icon" src="images/icon-heart.png" data-like="${posts[i].uuid}">
-    <img  class="react-icon" src="images/icon-comment.png">
-    <img class="react-icon" src="images/icon-dm.png">
-    </div>
-    
-    <div id="likes" class="likes">${posts[i].likes}</div>
-    <div id="caption"><strong>${posts[i].username} </strong>${posts[i].comment}</div>
-
-`   
-}
-postingEl.innerHTML=temp
-}
-renderImage();
-//for likes increment on double click and single //
-
-
 document.addEventListener('dblclick' ,function(e){
     if(e.target.dataset.react ){
         handleLikes(e.target.dataset.react)
@@ -89,7 +64,7 @@ function handleLikes(postsId){
     
     renderImage();
 }
-document.addEventListener('click' ,function(e){
+document.addEventListener('dblclick' ,function(e){
     if(e.target.dataset.like ){
         handleLikes(e.target.dataset.like)
     }
@@ -108,4 +83,34 @@ function handleLikes(postsId){
     
     renderImage();
 }
+
+let postingEl=document.getElementById("posting")
+function renderImage(){
+    let temp=""
+for(let i=0;i<posts.length;i+=1){
+    temp+=`
+
+    <div class="user-info">
+        <div id="post-1-dp"><img  id="post-1-dp" src="${posts[i].avatar}"></div>
+        <div class="info">
+            <div id="fname">${posts[i].name}</div>
+            <div  id="location">${posts[i].location}</div>
+        </div>
+    </div>
+    
+    <div class="post-1-image"><img id="post-1"  src="${posts[i].post}" data-react="${posts[i].uuid}"></div>
+    <div id="react">
+    <img  id="heart" class="react-icon" src="images/icon-heart.png" data-like="${posts[i].uuid}">
+    <img  class="react-icon" src="images/icon-comment.png" data-comment="${posts[i].uuid}">
+    <img class="react-icon" src="images/icon-dm.png" data-share="${posts[i].uuid}">
+    </div>
+    
+    <div id="likes" class="likes">${posts[i].likes}</div>
+    <div id="caption"><strong>${posts[i].username} </strong>${posts[i].comment}</div>
+
+`   
+}
+postingEl.innerHTML=temp
+}
+renderImage();
 
