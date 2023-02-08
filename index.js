2a40ecebe017c2889ba27f8ab34b4cb1e46daa6a
Shivam7264 <91426482+Shivam7264@users.noreply.github.com>
2023-02-08 14:16:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function handleLikes(postsId){
     
     renderImage();
 }
-document.addEventListener('dblclick' ,function(e){
+document.addEventListener('click' ,function(e){
     if(e.target.dataset.like ){
         handleLikes(e.target.dataset.like)
     }
